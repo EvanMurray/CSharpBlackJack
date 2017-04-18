5685a0f7f563ee7ed8d71b65d06f1b8b820d13da
Evan Murray <emurray728@gmail.com>
2017-04-18 00:04:22 -0400

    Initial Commit

---
+++ Blackjack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deck of cards
+            //dealer
+            //player
+            //hit or pass
+            //win condition
+            
+        }
+    }
+}
