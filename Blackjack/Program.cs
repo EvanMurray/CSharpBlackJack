b9c6e5142b0ea447da5a85b3c029385374c09d60
Evan Murray <emurray728@gmail.com>
2017-04-18 00:44:38 -0400

    Deck now populated with cards

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -11,11 +11,53 @@ namespace Blackjack
         static void Main(string[] args)
         {
             //deck of cards
+
+            Card[] deck = new Card[52];
+
+            populateDeck(deck);
+            
             //dealer
             //player
             //hit or pass
             //win condition
-            
+
+         }
+
+       
+
+        private static void populateDeck(Card[] deck)
+        {
+            int count = 0;
+
+            for(int suit = 1; suit <= 4; suit ++)
+            {
+                
+
+                for(int value = 1; value <= 13; value ++)
+                {
+                    if(suit == 1)
+                    {
+                        deck[count] = new Card() {Suit = "Spades", Value = value };
+                        count++;
+                    }
+                    else if (suit == 2)
+                    {
+                        deck[count] = new Card() { Suit = "Clubs", Value = value };
+                        count++;
+                    }
+                    else if (suit == 3)
+                    {
+                        deck[count] = new Card() { Suit = "Diamonds", Value = value };
+                        count++;
+                    }
+                    else if (suit == 4)
+                    {
+                        deck[count] = new Card() { Suit = "Hearts", Value = value };
+                        count++;
+                    }
+
+                }
+            }
         }
     }
 }
