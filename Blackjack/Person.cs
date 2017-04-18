90303ec86e6b200349669cf903eb4e180d52c27f
Evan Murray <emurray728@gmail.com>
2017-04-18 01:51:17 -0400

    Added the beginnings of the game with player and dealer recieving cards

---
+++ Blackjack/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class Person
+    {
+        public List<Card> Hand { get; set; }
+        public int Score { get; set; }
+
+        public Person()
+        {
+            Hand = new List<Card>();
+        }
+    }
+}
--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -12,50 +12,122 @@ namespace Blackjack
         {
             //deck of cards
 
-            Card[] deck = new Card[52];
+            List<Card> deck = new List<Card>(52);
 
             populateDeck(deck);
-            
+
             //dealer
+            Person dealer = new Person();
             //player
+
+            Person player = new Person();
+
+            game(deck, player, dealer);
             //hit or pass
             //win condition
-
+            
          }
+        
+       private static void game(List<Card> deck, Person player, Person dealer)
+        {
+            
+            bool isPlayersTurn = true;
+            bool isDealersTurn = false;
+            
+            Random rand = new Random();
+
+            while (true)
+            {
+                if(isPlayersTurn)
+                {
+                    Card c = deck[rand.Next(deck.Count)];
+                    player.Hand.Add(c);
+                    Console.WriteLine("Card: " + c.Value);
+                    deck.Remove(c);
+                    player.Score += c.Value;
+                    Console.WriteLine("Player Score: " + player.Score);
+                    if(player.Score > 21)
+                    {
+                        Console.WriteLine("You've Lost!");
+                        break;
+                    }
+                    Console.WriteLine("1: Hit || 2: Stay ");
+                    int hit = int.Parse(Console.ReadLine());
+                    if(hit == 1)
+                    {
+                        continue;
+                    }
+                    else if(hit == 2)
+                    {
+                        isPlayersTurn = false;
+                        isDealersTurn = true;
+                    }
+                    
+                }
+                else if(isDealersTurn)
+                {
+                    Card c = deck[rand.Next(deck.Count)];
+                    dealer.Hand.Add(c);
+                    deck.Remove(c);
+                    dealer.Score += c.Value;
+                    Console.WriteLine("Dealer Score: " + dealer.Score);
+                    if (dealer.Score < 16)
+                    {
+                        continue;
+                    }
+                    else if (dealer.Score >= 16 && dealer.Score <= 21)
+                    {
+                        if (dealer.Score >= player.Score)
+                        {
+                            Console.WriteLine("You Lose!");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You won!");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You won!");
+                        break;
+                    }   
+                }  
+            }     
+        }
 
        
 
-        private static void populateDeck(Card[] deck)
+        private static void populateDeck(List<Card> deck)
         {
             int count = 0;
 
             for(int suit = 1; suit <= 4; suit ++)
-            {
-                
+            {      
 
                 for(int value = 1; value <= 13; value ++)
                 {
                     if(suit == 1)
                     {
-                        deck[count] = new Card() {Suit = "Spades", Value = value };
+                        deck.Add(new Card() {Suit = "Spades", Value = value });
                         count++;
                     }
                     else if (suit == 2)
                     {
-                        deck[count] = new Card() { Suit = "Clubs", Value = value };
+                        deck.Add(new Card() { Suit = "Clubs", Value = value });
                         count++;
                     }
                     else if (suit == 3)
                     {
-                        deck[count] = new Card() { Suit = "Diamonds", Value = value };
+                        deck.Add(new Card() { Suit = "Diamonds", Value = value });
                         count++;
                     }
                     else if (suit == 4)
                     {
-                        deck[count] = new Card() { Suit = "Hearts", Value = value };
+                        deck.Add(new Card() { Suit = "Hearts", Value = value });
                         count++;
                     }
-
                 }
             }
         }
